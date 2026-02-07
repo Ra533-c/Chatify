95bab3c8fe06039dc53fbe1019f3cbb5c410d9cd
Rajnigoswami <rajnikantgoswami787@gmail.com>
2026-02-07 20:29:39 +0530

    Fix: Socket.io CORS - use dynamic CLIENT_URL for production

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,39 +1,42 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import express from "express";
 import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express()
 
 const server = http.createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin:"http://localhost:5173",
-        methods:['GET','POST'],
-        credentials:true
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        methods: ['GET', 'POST'],
+        credentials: true
     }
 })
 
-export const getReceiverSocketId = (receiverId)=>{
+export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
 }
 
 const userSocketMap = {};  //{userId -> socketId}
 
-io.on('connection',(socket)=>{
-    console.log("user connected with Socket.id :-",socket.id);
+io.on('connection', (socket) => {
+    console.log("user connected with Socket.id :-", socket.id);
     const userId = socket.handshake.query.userId;
-    if(userId !== undefined){
+    if (userId !== undefined) {
         userSocketMap[userId] = socket.id;
-        console.log("userSocketMap:-",userSocketMap);
+        console.log("userSocketMap:-", userSocketMap);
     }
 
-    io.emit('getOnlineUsers',Object.keys(userSocketMap));
-    
-    socket.on("disconnect",()=>{
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
         delete userSocketMap[userId];
-        io.emit('getOnlineUsers',Object.keys(userSocketMap));
-        console.log("user disconnected with Socket.id :-",socket.id);
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+        console.log("user disconnected with Socket.id :-", socket.id);
     });
 });
 
-export {app,server,io};
+export { app, server, io };
