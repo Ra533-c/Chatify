66b87a2756511331f9d9f38561c636e00cc1e063
Rajnigoswami <rajnikantgoswami787@gmail.com>
2026-02-08 21:09:22 +0530

    Feature: Mobile responsive scaling

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,9 +4,9 @@ import MessageContainer from "./MessageContainer";
 
 const Homepage = () => {
     return (
-        <div className="flex flex-col items-center gap-4 md:flex-row">
+        <div className="flex flex-col items-center gap-4">
             <h1 className="text-4xl font-bold text-white">Chatify</h1>
-            <div className="flex overflow-auto sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60 border border-gray-100">
+            <div className="flex overflow-auto h-[85vh] sm:h-[450px] md:h-[550px]  rounded-lg overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60 border border-gray-100">
                 <Sidebar />
                 <MessageContainer />
             </div>
